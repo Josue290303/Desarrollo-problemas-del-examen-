af50cc9928630782063277fbceb6aebf6da37090
Josue290303 <josue.lopez@upeu.edu.pe>
2020-10-27 19:44:00 +0000

    ejercicio3

---
+++ Examen/Ejercicio3.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+	g = float()
+	h = float()
+	m = float()
+	print(vacunaa,vacunab,vacunac)
+	print(" TiposVacunas")
+	vacunac = g+70
+	vacunaa = h+69
+	vacunab = m+69
+	vacunaa = g-16
+	print("QueVacunaAplicar")
+	vacunaa = (h+69)+(g-16)
+	vacunab = (m+69)
+	vacunac = g+70
