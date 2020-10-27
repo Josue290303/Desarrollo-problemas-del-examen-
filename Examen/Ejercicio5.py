fd3c4c87c0b4007c4ac9326dcd7c97b39507f0a0
Josue290303 <josue.lopez@upeu.edu.pe>
2020-10-27 15:33:23 +0000

    Ejercicio

---
+++ Examen/Ejercicio5.py
@@ -0,0 +1,12 @@
+
+def IncrementoAlSalarioDeUnProfesor():
+    for i in range(1,7):
+        print("PROCESO ",i)
+        salario_inicial = 1400
+        ano = i
+        salario_recibido = salario_inicial*(1.1)**(ano)
+        print("Valor de salario inicial: ",salario_inicial)
+        print("Valor de salario recibido: ",salario_recibido)
+        print("Valor de año: ",ano)
+        print("")
+IncrementoAlSalarioDeUnProfesor()
