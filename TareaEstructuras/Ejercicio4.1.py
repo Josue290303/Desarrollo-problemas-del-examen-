db8bdee8dee0f55e98de5070728ef85599ab778f
Josue290303 <josue.lopez@upeu.edu.pe>
2020-10-28 01:54:19 +0000

    ejercicio4.1

---
+++ TareaEstructuras/Ejercicio4.1.py
@@ -0,0 +1,10 @@
+for i in range(1,7):
+		print("PROCESO ",i)
+		salario_inicial = 1500
+		year = i
+		salario_recibido = salario_inicial*(1.1)**(year)
+		print("Valor de salario inicial: ",salario_inicial)
+		print("Valor de salario recibido: ",salario_recibido)
+		print("Valor de year: ",year)
+		print("")
+
