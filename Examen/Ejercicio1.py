e53c280fe6b0ac73c3265179afe964b229022767
Josue290303 <josue.lopez@upeu.edu.pe>
2020-11-01 22:24:24 +0000

    ejercicio1

--- Examen/Ejercicio1.py
+++ Examen/Ejercicio1.py
@@ -1,15 +1,12 @@
 if __name__ == '__main__':
-	print("Ingrese Salario mínimo:")
-	salario = float(input())
-	print("Ingrese puntos:")
-	puntos = input()
-	if puntos<=50 and puntos<=100:
-		bono = salario*0.1
-	else:
-		if puntos<=101 and puntos<=150:
-			bono = salario*0.5
-		else:
-			if puntos>=151:
-				bono = salario*0.8
-	print("Bono:",bono)
+	print("Ingrese nota 1ra Unidad:")
+	nota1 = float(input())
+	print("Ingrese nota 2da Unidad")
+	nota2 = float(input())
+	print("Ingrese nota 3ra Unidad")
+	nota3 = float(input())
+	print("Ingrese nota Trabajo Final")
+	trabajofinal = float(input())
+	notafinal = 0.1*nota1+0.15*nota2+0.25*nota3+0.5*trabajofinal
+	print("Nota Final:",notafinal)
 
---
+++ Examen/Ejercicio2.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+	print("Ingrese Salario mínimo:")
+	salario = float(input())
+	print("Ingrese puntos:")
+	puntos = input()
+	if puntos<=50 and puntos<=100:
+		bono = salario*0.1
+	else:
+		if puntos<=101 and puntos<=150:
+			bono = salario*0.5
+		else:
+			if puntos>=151:
+				bono = salario*0.8
+	print("Bono:",bono)
+
