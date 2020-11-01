d2cb0a85722ae6b79e89dab18763b0f02c8e5b4a
Josue290303 <josue.lopez@upeu.edu.pe>
2020-11-01 21:36:51 +0000

    ejercicio5

--- Examen/Ejercicio5.py
+++ Examen/Ejercicio5.py
@@ -1,12 +1,16 @@
+if __name__ == '__main__':
+	anos = 6
+	salario_inicial = 1400
+	porcentajeincremento = 1.1
+	for i in range(1,anos+1):
+		print("PROCESO ",i)
+		ano = i
+		salario_recibido = salario_inicial*(porcentajeincremento)**(ano)
+		print("Valor de salario inicial: ",salario_inicial)
+		print("Valor de salario recibido: ",salario_recibido)
+		print("Valor de año: ",ano)
+		print("")
+		if i==6:
+			salario_final = salario_recibido
+	print("Salario Final:",salario_final)
 
-def incrementoAlSalarioDeUnProfesor():
-    for i in range(1,7):
-        print("PROCESO ",i)
-        salario_inicial = 1400
-        ano = i
-        salario_recibido = salario_inicial*(1.1)**(ano)
-        print("Valor de salario inicial: ",salario_inicial)
-        print("Valor de salario recibido: ",salario_recibido)
-        print("Valor de año: ",ano)
-        print("")
-incrementoAlSalarioDeUnProfesor()
